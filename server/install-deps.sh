efbd76408f15228a86d4983859c1af9b09b666a9
Jan Kunzmann <jan-github@phobia.de>
2018-01-24 00:29:51 +0100

    PoC server

---
+++ server/install-deps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SENSU_PLUGINS=(
+  # irc
+  # logstash
+  telegram
+)
+
+set -e
+
+apt-get update
+apt-get install -y libxml2 libxml2-dev libxslt1-dev zlib1g-dev build-essential
+
+# Install Plugins
+PARALLEL_INSTALLATION=0 UNINSTALL_BUILD_TOOLS=0 /bin/install ${SENSU_PLUGINS[@]}
+
+
+apt-get remove -y libxml2 libxml2-dev libxslt1-dev zlib1g-dev build-essential
+apt-get autoremove -y
+
+rm -rf /var/lib/apt/lists/*
